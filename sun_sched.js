a10b30376243d6d674018fad691c534a0d5c048a
Yorick van Pelt <yorickvanpelt@gmail.com>
2013-11-18 13:05:02 +0100

    weekday schedule, remove first

--- sun_sched.js
+++ sun_sched.js
@@ -7,20 +7,46 @@ var airport = require('airport')
 var air = airport('frumar.yori.cc', 9090)
 var lights = air.connect('lightsd@0.0.2')
 
-function addForDate(d, remote) {
+function startTime(d) {
 	var times = SunCalc.getTimes(d, 52.22, 5.155)
+	return times.sunsetStart }
+function endTime(d) {
 	var end = new Date(d)
-	end.setDate(end.getDate() + 1)
-	end.setHours(0)
-	end.setMinutes(0)
-	remote.schedule.addLight(+times.sunsetStart, "schoorsteen", true, "sunset")
-	remote.schedule.addLight(+end, "schoorsteen", false, "night")
-	console.log("on: ", times.sunsetStart)
+	switch(d.getDay()) {
+		case 0: case 1: case 2: case 3: case 4:
+			end.setHours(23)
+			end.setMinutes(15)
+			break
+		default:
+			end.setDate(end.getDate() + 1)
+			end.setHours(0)
+			end.setMinutes(0)
+			break }
+	return end }
+function dateRepr(d) {
+	return [d.getFullYear(), d.getMonth(), d.getDate()].join("-") }
+
+function addForDate(d, remote) {
+	var start = startTime(d)
+	var end   = endTime(d)
+	var repr  = dateRepr(d)
+	remote.schedule.addLight(+start, "schoorsteen", true, "sunset " + repr)
+	remote.schedule.addLight(+end, "schoorsteen", false, "night " + repr)
+	console.log("on: ", start)
 	console.log("off: ", end)
 }
+function remForDate(d, remote) {
+	var repr  = dateRepr(d)
+	remote.schedule.list(function(sched) {
+		Object.keys(sched).forEach(function(id) {
+			if (sched[id].desc == "sunset " + repr ||
+				sched[id].desc == "night "  + repr)
+				remote.schedule.del(id) })})}
 
-lights(function(r) {
-	addForDate(new Date, r)
+lights(function(r, conn) {
+	var d = new Date
+	remForDate(d, r)
+	addForDate(d, r)
 	setTimeout(process.exit, 1000)
 })
 
