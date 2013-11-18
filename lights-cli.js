5bf2d1f9a8519786899f8064067e74b7fcefa588
Yorick van Pelt <yorickvanpelt@gmail.com>
2013-11-18 01:56:12 +0100

    add CLI utility

---
+++ lights-cli.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+
+	// @list-schedule = list-schedule
+	// @del-schedule = del-schedule
+	// @add-schedule = add-schedule
+	// @add-schedule-light = add-schedule-light
+
+var airport = require('airport')
+var air = airport('frumar.yori.cc', 9090)
+var lights = air.connect('lightsd@0.0.2')
+if (process.argv.length < 3) {
+	console.log("no command given")
+}
+else {
+	lights(function(r) {
+		switch (process.argv[2]) {
+			case "list":
+				r.lights.list(function(l) {
+					Object.keys(l).forEach(function(k) {
+						process.stdout.write(l[k].name + " " + "\n")
+					})
+					process.exit()
+				})
+				break;
+			case "on":
+			case "off":
+				if (process.argv.length == 4) {
+					r.lights.turn(process.argv[3], process.argv[2] == "on", cb)
+				} else {
+					r.turn(process.argv[3], process.argv[4], process.argv[2] == "on", cb)
+				}
+				function cb(st) {
+					if (st) {
+						process.stdout.write("OK")
+					} else if (st === false) {
+						process.stdout.write("ERR")
+					}
+					process.exit()
+				}
+				break;
+			case "add":
+				r.lights.add(process.argv[3], process.argv[4], process.argv[5], process.exit)
+				break;
+			case "del":
+				r.lights.delName(process.argv[3], process.exit)
+				break;
+			default:
+				process.stderr.write("Error: unknown command")
+				process.exit()
+		}
+	})
+}
